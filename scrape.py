72bf73ef368488393f4a066178fc316e03ed9daf
Jonathanace <jonathanace@gmail.com>
2022-06-22 22:13:43 -0700

    test

--- scrape.py
+++ scrape.py
@@ -5,14 +5,6 @@ from bs4 import BeautifulSoup
 
 start_time = time.time()
 
-
-
-
-
-
-
-
-
 def scrape_page():
     threading.Timer(10.0, scrape_page).start() # recursively calls scrape function every 10 seconds
     print(f'"scrape called": {round(time.time() - start_time, 2)} seconds')
