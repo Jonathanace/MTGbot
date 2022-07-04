5af1de9c0f1673a2d6f28911a374416665980ae5
Jonathanace <jonathanace@gmail.com>
2022-07-04 11:33:28 -0700

    commit

---
+++ config.py
@@ -0,0 +1,18 @@
+# TCGPlayer fees
+marketplace_comission = 9.25 # percent
+credit_card_precent_fee = 2.5 # percent
+credit_card_flat_fee = 0.30 # flat
+
+# shipping costs
+stamp_cost = 0.51 # flat
+letter_cost = 0.05 # flat
+toploader = 0.33 # flat
+bubble_mailer = 0.13 # flat
+shipping_label = 3.49 # flat
+
+# other
+target_margin = 6 # percent
+sales_tax = 10.25 # percent
+blacklisted_sets = []
+blacklisted_cards = []
+
